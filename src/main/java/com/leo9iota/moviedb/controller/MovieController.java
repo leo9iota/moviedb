d4a6b22279d834818b749ff40d40a9c8f653e9e5
leo9iota <nine_iota1@proton.me>
2022-11-26 21:38:01 +0100

    Add MovieController.java

---
+++ src/main/java/com/leo9iota/moviedb/controller/MovieController.java
@@ -0,0 +1,30 @@
+package com.leo9iota.moviedb.controller;
+
+import com.leo9iota.moviedb.model.Movie;
+import com.leo9iota.moviedb.repository.MovieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class MovieController {
+
+    @Autowired
+    private MovieRepository movieRepository;
+
+    @PostMapping("/create")
+    public void createMovie(@RequestBody Movie movie) {
+        movieRepository.insert(movie);
+    }
+
+    @PostMapping("/delete/{id}")
+    public void deleteMovie(@PathVariable String id) {
+        movieRepository.deleteById(id);
+    }
+
+    @GetMapping("/list")
+    public List<Movie> getMovies() {
+        return movieRepository.findAll();
+    }
+}
