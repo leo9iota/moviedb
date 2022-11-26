775b32cd5c62147abcde93c9827dc75062b860c5
leo9iota <nine_iota1@proton.me>
2022-11-26 19:18:38 +0100

    Initial commit

---
+++ src/main/java/com/leo9iota/moviedb/MoviedbApplication.java
@@ -0,0 +1,13 @@
+package com.leo9iota.moviedb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviedbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviedbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leo9iota/moviedb/MoviedbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leo9iota.moviedb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviedbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
